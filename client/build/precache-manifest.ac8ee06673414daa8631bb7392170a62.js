92ce358516c17faafe6d7df0e227db5629925fa7
tmagz <thomasmaglaris@gmail.com>
2020-10-28 23:03:12 +1030

    run build

---
+++ client/build/precache-manifest.ac8ee06673414daa8631bb7392170a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d272b2f44369b543bde10df112be33c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0fe08a076b1310e8f4e",
+    "url": "/static/css/main.1f96e148.chunk.css"
+  },
+  {
+    "revision": "3f036ae4269e4e484b0a",
+    "url": "/static/js/2.b0e857c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0e857c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0fe08a076b1310e8f4e",
+    "url": "/static/js/main.ec015d34.chunk.js"
+  },
+  {
+    "revision": "1cdcd5266f1a0418b704",
+    "url": "/static/js/runtime-main.4f02f0e0.js"
+  }
+]);
